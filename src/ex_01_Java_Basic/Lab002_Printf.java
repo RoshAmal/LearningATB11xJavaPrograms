766f38f5c0b3d35398032de68041bb89c3823158
RoshAmal <amalrosh707@gmail.com>
2025-04-05 22:18:05 +0530

    usage of printf

---
+++ src/ex_01_Java_Basic/Lab002_Printf.java
@@ -0,0 +1,18 @@
+package ex_01_Java_Basic;
+
+public class Lab002_Printf {
+    public static void main(String []args){
+        int table = 9;
+        System.out.printf("Table of %d:\n", table);
+        System.out.printf("%d x %d = %d\n", 1, table, table);
+        System.out.printf("%d x %d = %d\n", 2, table, 2*table);
+        System.out.printf("%d x %d = %d\n", 3, table, 3*table);
+        System.out.printf("%d x %d = %d\n", 4, table, 4*table);
+        System.out.printf("%d x %d = %d\n", 5, table, 5*table);
+        System.out.printf("%d x %d = %d\n", 6, table, 6*table);
+        System.out.printf("%d x %d = %d\n", 7, table, 7*table);
+        System.out.printf("%d x %d = %d\n", 8, table, 8*table);
+        System.out.printf("%d x %d = %d\n", 9, table, 9*table);
+        System.out.printf("%d x %d = %d\n", 10, table, 10*table);
+    }
+}
