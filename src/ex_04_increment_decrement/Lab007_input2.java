ecc76369a769e9891d539fcf2289d2b6cf461cc9
RoshAmal <amalrosh707@gmail.com>
2025-04-14 11:50:56 +0530

    scanner 2

---
+++ src/ex_04_increment_decrement/Lab007_input2.java
@@ -0,0 +1,13 @@
+package ex_04_increment_decrement;
+
+import java.util.Scanner;
+
+public class Lab007_input2 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int result = a > b ? a : b;
+        System.out.printf("Maximum among %d and %d is %d", a, b, result);
+    }
+}
