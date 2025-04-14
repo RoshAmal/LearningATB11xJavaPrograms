1cfe253168448b81f921cee9c265f42121e73159
RoshAmal <amalrosh707@gmail.com>
2025-04-14 12:05:55 +0530

    Even Odd

---
+++ src/ex_04_increment_decrement/Lab009_EvenOdd.java
@@ -0,0 +1,12 @@
+package ex_04_increment_decrement;
+
+import java.util.Scanner;
+
+public class Lab009_EvenOdd {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int no = sc.nextInt();
+        String result = no%2==0 ? "Even" : "Odd";
+        System.out.printf("%d is %s", no, result);
+    }
+}
