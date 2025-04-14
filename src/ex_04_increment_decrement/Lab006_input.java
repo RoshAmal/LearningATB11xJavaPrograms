4f09af8d6f17e382d366414c678e3925c8319d82
RoshAmal <amalrosh707@gmail.com>
2025-04-14 11:35:30 +0530

    scanner

---
+++ src/ex_04_increment_decrement/Lab006_input.java
@@ -0,0 +1,15 @@
+package ex_04_increment_decrement;
+
+import java.util.Scanner;
+
+public class Lab006_input {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();
+        int age = sc.nextInt();
+        double salary = sc.nextDouble();
+        System.out.printf("Name is %s\n", name);
+        System.out.printf("Age is %d\n", age);
+        System.out.printf("Salary is %.2f", salary);
+    }
+}
