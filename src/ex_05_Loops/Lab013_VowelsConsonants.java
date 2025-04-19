c109c9b4aaf05fe0dca8e5f345a447d9a9f1a037
RoshAmal <amalrosh707@gmail.com>
2025-04-19 20:24:54 +0530

    loops

---
+++ src/ex_05_Loops/Lab013_VowelsConsonants.java
@@ -0,0 +1,28 @@
+package ex_05_Loops;
+
+import java.util.Scanner;
+
+public class Lab013_VowelsConsonants {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next();
+        int vowels = 0;
+        int consonants = 0;
+        for (int i=0; i<input.length(); i++){
+            if(input.charAt(i) == 'a')
+                vowels++;
+            else if(input.charAt(i) == 'e')
+                vowels++;
+            else if(input.charAt(i) == 'i')
+                vowels++;
+            else if(input.charAt(i) == 'o')
+                vowels++;
+            else if(input.charAt(i) == 'u')
+                vowels++;
+            else
+                consonants++;
+        }
+        System.out.printf("The input string %s has %d vowels and %d consonants",
+                input, vowels, consonants);
+    }
+}
---
+++ src/ex_05_Loops/Lab014_Palindrome.java
@@ -0,0 +1,19 @@
+package ex_05_Loops;
+
+import java.util.Scanner;
+public class Lab014_Palindrome {
+    public static void main(String []args){
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next();
+        int inLength = input.length();
+        char[] output = new char[inLength];
+        int offset = inLength-1;
+        for (int i=0; i<inLength; i++){
+            output[i] = input.charAt(offset);
+            offset--;
+        }
+        System.out.printf("The palindrome of the input %s is ", input);
+        System.out.println(output);
+
+    }
+}
