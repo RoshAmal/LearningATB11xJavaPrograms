4697c8197db131d1b1ef605bcbbf47fa3d02c4f2
RoshAmal <amalrosh707@gmail.com>
2025-06-26 00:23:36 +0530

    p1

--- src/ex_16_WrapperClass/Lab062_program1.java
+++ src/ex_16_WrapperClass/Lab062_program1.java
@@ -5,19 +5,24 @@ import java.util.Scanner;
 public class Lab062_program1 {
     public static void main(String[] args){
         int small = 3;
+        // auto-boxing
         Integer big = small;
+        // unboxing
         int small2 = big.intValue();
         System.out.println(small2);
 
 
         double value = 3.14159;
+        // auto-boxing
         Double boxedValue = value;
+        // wrapper to String
         String strDouble = boxedValue.toString();
         System.out.println(strDouble.length());
 
         Scanner sc = new Scanner(System.in);
         String input = sc.next();
-        Integer in = Integer.parseInt(input);
+        // String to primitive
+        int in = Integer.parseInt(input);
         System.out.println(in*in);
         sc.close();
 
@@ -27,6 +32,14 @@ public class Lab062_program1 {
         Integer d = 128;
         System.out.println(a==b);
         System.out.println(c==d);
+
+        int strSum = 0;
+        String strInput = args[0];
+        for (int i=0; i < strInput.length(); i++){
+            char charA = strInput.charAt(i);
+            strSum += Character.getNumericValue(charA);
+        }
+        System.out.println(strSum);
 
     }
     
